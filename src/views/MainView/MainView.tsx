1f361f0f7b2f8e9e2815c05528f68f5fd55a3e47
conorhutchins <conorhutchins@btinternet.com>
2024-03-26 20:49:28 +0000

    creates custom hook useWeatherApi and changes conditional rendering to be neater

--- src/views/MainView/MainView.tsx
+++ src/views/MainView/MainView.tsx
@@ -1,22 +1,10 @@
 import React, { useEffect, useState} from "react";
 import CityWeatherCard from "../../components/CityWeatherCard/CityWeatherCard";
 import styles from "../styles/MainView.module.css";
-import { fetchWeatherData, City } from "../../api/weather";
+import { useWeatherApi } from "../../api/hooks/useWeatherApi";
 
 const MainView: React.FC = () => {
-    const [cities, setCities] = useState<City[]>([]);
-    const [loading, setLoading] = useState<boolean>(true);
-    const [error, setError] = useState<string | null>(null);
-
-    useEffect(()=> {
-    fetchWeatherData().then(data => {
-        setCities(data);
-        setLoading(false);
-    }).catch(error => {
-        setError(error.message);
-        setLoading(false);
-    })
-    }, []);
+  const { cities, loading, error } = useWeatherApi();
 
     if (loading) {
         return <div>Still loading...</div>;
@@ -26,19 +14,21 @@ const MainView: React.FC = () => {
         return <div>{error}</div>;
     }
     return (
-        <div className={styles.mainView}>
-          {cities.map((city) => (
-            <CityWeatherCard 
-              key={city.name}
-              name={city.name}
-              temperature={city.weather.temperature}
-              conditions={city.weather.conditions}
-              windSpeed={city.weather.windSpeed}
-              humidity={city.weather.humidity} 
-            />
-          ))}
-        </div>
-      );
-    };
+      <div className={styles.mainView}>
+        {loading && <div>Still loading...</div>}
+        {error && <div>{error}</div>}
+        {!loading && !error && cities.map((city) => (
+          <CityWeatherCard 
+            key={city.name}
+            name={city.name}
+            temperature={city.weather.temperature}
+            conditions={city.weather.conditions}
+            windSpeed={city.weather.windSpeed}
+            humidity={city.weather.humidity} 
+          />
+        ))}
+      </div>
+    );
+  };
 
     export default MainView;
