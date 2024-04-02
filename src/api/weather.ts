771f003fe0f6ae5a05378c1da64e3fd96fda52a4
conorhutchins <conorhutchins@btinternet.com>
2024-04-02 12:35:52 +0100

    prettifies weather.ts and MainView.modules.css

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -1,26 +1,28 @@
 export interface Weather {
-    city: string;
-    temperature: string;
-    conditions: string
-    windSpeed: string;
-    humidity: string;
-    }
-    
-export interface City {
-        name: string;
-        weather: Weather;
-    }
-
-export interface WeatherData {
-    location: string, 
-    cities: City[]
+  city: string;
+  temperature: string;
+  conditions: string;
+  windSpeed: string;
+  humidity: string;
 }
 
-  export async function fetchWeatherData(): Promise<City[]> {
-    const response = await fetch('https://raw.githubusercontent.com/WillPlayground/weather-data/main/uk-weather.json');
-    if (!response.ok) {
-      throw new Error("Failed to fetch data");
-    }
+export interface City {
+  name: string;
+  weather: Weather;
+}
+
+export interface WeatherData {
+  location: string;
+  cities: City[];
+}
+
+export async function fetchWeatherData(): Promise<City[]> {
+  const response = await fetch(
+    "https://raw.githubusercontent.com/WillPlayground/weather-data/main/uk-weather.json"
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
   const rawData = await response.json();
   const data: WeatherData = {
     location: rawData.location,
@@ -31,9 +33,9 @@ export interface WeatherData {
         temperature: city.weather.temperature,
         conditions: city.weather.conditions,
         windSpeed: city.weather.wind_speed,
-        humidity: city.weather.humidity
-      }
-    }))
-  }
+        humidity: city.weather.humidity,
+      },
+    })),
+  };
   return data.cities;
-  }
+}
