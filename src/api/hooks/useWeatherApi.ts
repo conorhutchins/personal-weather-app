81f49949ccb2b448ac9fbcff2b969c3b53cafaaf
conorhutchins <conorhutchins@btinternet.com>
2024-04-02 21:39:21 +0100

    initial test of MainView, some problems with MainView

--- src/api/hooks/useWeatherApi.ts
+++ src/api/hooks/useWeatherApi.ts
@@ -1,19 +1,27 @@
-import { useEffect, useState } from 'react';
-import { City, fetchWeatherData } from '../weather';
+import { useEffect, useState } from "react";
+import { City, fetchWeatherData } from "../weather";
 
 export const useWeatherApi = () => {
-    const [cities, setCities] = useState<City[]>([]);
-    const [loading, setLoading] = useState<boolean>(true);
-    const [error, setError] = useState<string | null>(null);
+  const [cities, setCities] = useState<City[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
-    useEffect(()=> {
-    fetchWeatherData().then(data => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const data = await fetchWeatherData();
         setCities(data);
+      } catch (error: unknown) {
+        const errorMessage =
+          error instanceof Error ? error.message : "An unknown error occurred";
+        setError(errorMessage);
+      } finally {
         setLoading(false);
-    }).catch(error => {
-        setError(error.message);
-        setLoading(false);
-    })
-    }, []);
-    return { cities, loading, error };
-}
+      }
+    };
+    fetchData();
+  }, []);
+
+  return { cities, loading, error };
+};
