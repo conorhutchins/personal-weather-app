d0dd14c041af7e1977b2abdd062e19f8effe318e
conorhutchins <conorhutchins@btinternet.com>
2024-05-31 17:54:38 +0100

    adds assets and uses them in mainview

--- src/views/MainView/MainView.tsx
+++ src/views/MainView/MainView.tsx
@@ -3,6 +3,21 @@ import CityWeatherCard from "../../components/CityWeatherCard/CityWeatherCard";
 import styles from './MainView.module.css';
 import { useWeatherApi } from "../../api/hooks/useWeatherApi";
 import { useNavigate } from "react-router-dom";
+import sunIcon from '../../assets/images/sunny.png';
+import cloudIcon from '../../assets/images/cloudy.png';
+import rainIcon from '../../assets/images/rain.png';
+import backIcon from '../../assets/images/back.png';
+import partialCloudIcon from '../../assets/images/partially-cloudy.png';
+import notFound from '../../assets/images/not-found.png';
+
+
+const weatherIcons: {[key: string]: string | undefined } = {
+  Sunny: sunIcon,
+  Cloudy: cloudIcon,
+  Rain: rainIcon,
+  'Partly Cloudy': partialCloudIcon,
+  'Not Found': notFound,
+};
 
 export const MainView: React.FC = () => {
   const { cities, loading, error } = useWeatherApi();
@@ -17,15 +32,12 @@ export const MainView: React.FC = () => {
     }
     return (
       <div className={styles.mainView}>
+        <h1> Weather United Kingdom </h1>
+        <div className="time">{new Date().toLocaleTimeString()}</div>
         {cities.map((city) => (
-          <div key={city.name}>
-          <CityWeatherCard 
-            name={city.name}
-            temperature={city.weather.temperature}
-            conditions={city.weather.conditions}
-            windSpeed={city.weather.windSpeed}
-            humidity={city.weather.humidity} 
-          />
+          <div key={city.name} className={styles.city}>
+          <h2>{city.name}</h2>
+          <img src ={weatherIcons[city.weather.conditions || notFound]} alt={city.weather.conditions} className={styles.weatherIcon}/>
           <button onClick={() => navigate(`/details/${city.name}`, { state: { city }})}>Details</button>
       </div>
         ))}
