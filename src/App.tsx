8bf0f08d1f0ad14643d6ca67295ad8e4a8be01e5
conorhutchins <conorhutchins@btinternet.com>
2024-06-13 20:06:31 +0100

    centers container vertically and aligns time and uk on same line

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css';
 import { MainView } from './views/MainView/MainView';
 import { DetailsView } from './views/DetailsView/DetailsView'
-// import Navbar from './components/Navbar'
-// import Footer from './components/Footer'
-// import { WeatherProvider } from './context/WeatherProvider'
 import './App.css'
 
 function App() {
@@ -13,8 +10,10 @@ function App() {
     <Router>
   <div className="container">
   <h1 className="weather">Weather</h1>
+    <div className="header">
   <h1 className="UK">United Kingdom</h1>
   <div className="time">{new Date().toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit', hour12: true })}</div>
+  </div>
     <Routes>
       <Route path="/" element={<MainView />}/>
       <Route path="details/:cityName" element={<DetailsView />}/>
