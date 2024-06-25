4269c40a951f2baf0ff48427c8a2270eeb37bdaf
conorhutchins <conorhutchins@btinternet.com>
2024-06-25 23:57:35 +0100

    add testing for app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,37 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Weather App/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('./views/MainView/MainView', () => ({
+  MainView: () => <div>MainView Component</div>,
+}));
+
+jest.mock('./views/DetailsView/DetailsView', () => ({
+  DetailsView: () => <div>DetailsView Component</div>,
+}));
+
+describe('App', () => {
+  it('renders the header correctly', () => {
+    render(<App />);
+
+    expect(screen.getByText('Weather')).toBeInTheDocument();
+    expect(screen.getByText('United Kingdom')).toBeInTheDocument();
+    expect(screen.getByText(/^\d{2}:\d{2}\s[AP]M$/i)).toBeInTheDocument(); // Matches the time format
+  });
+
+  it('renders MainView by default', () => {
+    window.history.pushState({}, 'MainView', '/');
+    render(<App />);
+
+    expect(screen.getByText('MainView Component')).toBeInTheDocument();
+  });
+
+  it('navigates to DetailsView when a link is clicked', async () => {
+    window.history.pushState({}, 'DetailsView', '/details/test-city');
+    render(<App />);
+
+    expect(screen.getByText('DetailsView Component')).toBeInTheDocument();
+  });
 });
